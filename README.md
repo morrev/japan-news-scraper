9234049f6f09154886440cc90baa9c2b09d20bf1
morrev <70282228+morrev@users.noreply.github.com>
2020-08-26 20:12:48 -0400

    Added kanji frequency count, minor Readme change

--- README.md
+++ README.md
@@ -3,6 +3,19 @@ japan-news-scraper
 
 準備
 ----
+``` python
+python scraper.py
+```
 
 分析
 ----
+
+### WIP
+``` python
+```
+
+Future considerations
+----
+- Determining word boundaries for Japanese text? (e.g. 国勢調査 > 国勢　調査)
+- Time series correlation between Nikkei 225 and sentiment of characters
+- Similarity between most frequent characters across newspapers (and countries)
--- scraper.py
+++ scraper.py
@@ -2,29 +2,44 @@
 
 from urllib.request import urlretrieve
 from os.path import isfile
+from collections import Counter
+import re
 from bs4 import BeautifulSoup as BS
 
-def get_titles(soup):
-    """Return text from article titles, with ideographic space stripped."""
-    titles = soup.select('span[class*="k-card__title-piece"]')
-    title_text = [t.text.strip().replace('\u3000','') for t in titles]
-    return title_text
+def get_title_elements(soup):
+    """Get title elements from the homepage"""
+    title_elements = soup.select('span[class*="k-card__title-piece"]')
+    return title_elements
+
+def parse_titles(title_elements):
+    """Get list of titles (text) and Counter of kanji (across all titles)"""
+    titles = set()
+    kanji_cnt = Counter()
+    for title in title_elements:
+        clean_title = title.text.strip().replace('\u3000', '') #strip ideographic space
+        titles.add(clean_title)
+        kanji_cnt += Counter(extract_kanji(clean_title))
+    return titles, kanji_cnt
 
 def get_nikkei225(soup):
     """Return Nikkei index."""
     nikkei225 = soup.select_one('span[class*="k-hub-market__current-price"]').text
     return nikkei225
 
+def extract_kanji(text):
+    """Extract only kanji from the title."""
+    regex = re.compile(u'[^\u4E00-\u9FFF]+') #CJK Unified Ideographs (kanji unicode)
+    return regex.sub('', text)
+
 def main():
     """Retrieve and summarize titles from Nikkei Keizai Shimbun."""
-    
     if not isfile('nikkei.html'):
         urlretrieve("https://www.nikkei.com/", "nikkei.html")
     soup = BS(open('nikkei.html'), 'html.parser')
-    
-    title_text = get_titles(soup)
+    title_elements = get_title_elements(soup)
+    titles, kanji_cnt  = parse_titles(title_elements)
     nikkei225 = get_nikkei225(soup)
-    print(title_text)
+    print(kanji_cnt)
     print(nikkei225)
 
 if __name__ == "__main__":
